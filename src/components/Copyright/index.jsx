154edefe2383fe93a1c30b32d4f7bf5f85c0885f
fernandezAlex <70136337+fernandezAlex@users.noreply.github.com>
2021-04-08 16:28:08 +0200

    SIGN IN page

---
+++ src/components/Copyright/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+        {'Created by © Alex Fernández '}
+        {new Date().getFullYear()}
+        {'.'}
+        </Typography>
+    );
+}
+
+export default Copyright;
